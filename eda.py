991d855de155bb5a403839ee48af7c025b55c7b2
Nick <nickdavidross@gmail.com>
2023-11-27 14:51:23 +0000

    Tests

---
+++ eda.py
@@ -0,0 +1,35 @@
+#%% libraries
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
+#%%
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+
+for idx, item in enumerate(results['items']):
+
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+
+# %%
+
+artist = sp.artist("066X20Nz7iquqkkCW6Jxy6")
+print(artist)
+
+
+
+
+# %%
+
+track = results['items']
+
+
+
+# %%
