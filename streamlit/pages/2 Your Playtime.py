3493061baee311ed358b26689d873d3622a8ab20
Nick <nickdavidross@gmail.com>
2024-03-19 14:22:24 +0000

    playtime chart

--- streamlit/pages/2 Your Playtime.py
+++ streamlit/pages/2 Your Playtime.py
@@ -19,27 +19,44 @@ st.subheader("What was your most musical year?")
 if "data" not in st.session_state or st.session_state.data is None:
     st.error("Please upload your listening history or check the 'testing' box in the 'Upload your data' tab.")
 
+
 else:
     data = st.session_state.data
 
-    total['month_label'] = total['period'].dt.strftime('%b')
+    # format dates
+    data_aggregated = data.sort_values(['datetime'], ascending= [True])
+    data_aggregated['month_label'] = data_aggregated['datetime'].dt.strftime('%b')
+    data_aggregated['month_number'] = data_aggregated['datetime'].dt.strftime('%m').astype("string")
 
-    n_years = 10
+    # group and sum playtime
+    data_aggregated = data_aggregated[["month_number", "month_label", "year","hours_played"]].groupby(["month_number", "month_label","year"])["hours_played"].sum().reset_index()
 
-    total_filtered = total[total['year'].astype("int") > max(total['year'].astype("int")) - n_years]
+    # sort
+    data_aggregated = data_aggregated.sort_values(['year','month_number'], ascending= [True, True])
 
-    total_fig = px.line(
-        total_filtered,
+    # cumulative sum
+    data_aggregated['hours_played_cumlative'] = data_aggregated.groupby(["year"])['hours_played'].cumsum()
+
+
+    # filter
+    year_selection = st.checkbox("Select years", data_aggregated.year.unique())
+
+    chart_data = data_aggregated[data_aggregated.year.isin(year_selection)]
+    
+
+    # plot
+    playtime_figure = px.line(
+        chart_data,
         x = "month_label",
-        y = "playtime_cumulative",
+        y = "hours_played_cumlative",
         color = "year",
         line_shape = "spline",
         height = 1000,
     )
 
     # add year labels
-    for i, d in enumerate(total_fig.data):
-        total_fig.add_scatter(x=[d.x[-1]], y = [d.y[-1]],
+    for i, d in enumerate(playtime_figure.data):
+        playtime_figure.add_scatter(x=[d.x[-1]], y = [d.y[-1]],
                         mode = 'markers+text',
                         text = d.legendgroup,
                         textfont = dict(color=d.line.color),
@@ -48,12 +65,12 @@ else:
                         legendgroup = d.name,
                         showlegend=False)
 
+    # theme
+    playtime_figure.layout.template = "plotly_dark+presentation+xgridoff"
+    playtime_figure.update_xaxes(title = None)
+    playtime_figure.update_yaxes(title = None)
+    playtime_figure.layout.legend.x = -0.3
 
-    total_fig.layout.template = "plotly_dark+presentation+xgridoff"
-    total_fig.update_xaxes(title = None)
-    total_fig.update_yaxes(title = None)
-    total_fig.layout.legend.x = -0.3
-
-    total_fig.show()
+    st.plotly_chart(playtime_figure)
 
 
